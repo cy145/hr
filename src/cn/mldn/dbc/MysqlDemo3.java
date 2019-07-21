f1b35535df487953d54e797dff90bc7ecd9dab13
cy145 <1459288522@qq.com>
2019-07-21 18:12:00 +0800

    增加了MysqlDemo3

---
+++ src/cn/mldn/dbc/MysqlDemo3.java
@@ -0,0 +1,29 @@
+package cn.mldn.dbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class MysqlDemo3{
+	private static final String DBDRIVER="org.gjt.mm.mysql.Driver";
+	private static final String DBURL="jdbc:mysql://localhost:3306/mldn";
+	private static final String DBUSER="root";
+	private static final String PASSWORD="mysqladmin";
+	public static void main(String[] args) throws Exception{
+		Class.forName(DBDRIVER);
+		Connection conn=DriverManager.getConnection(DBURL, DBUSER, PASSWORD);
+		String sql="SELECT mid,name,age,salary,birthday,note FROM member limit ?,?";
+		PreparedStatement pstmt=conn.prepareStatement(sql);
+		int currentPage=1;
+		int lineSize=10;
+		pstmt.setInt(1,(currentPage-1)*lineSize);
+		pstmt.setInt(2, lineSize);
+		ResultSet rs=pstmt.executeQuery();
+		while(rs.next()) {
+			System.out.println(rs.getInt(1)+","+rs.getString(2)+","+rs.getString(6));
+		}
+		
+	}
+}
